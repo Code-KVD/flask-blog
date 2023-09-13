6905cb1d707b5e18d838b04ea7bfecea2c364bd3
KVD <kvd.sridhar.code@gmail.com>
2023-09-13 15:17:54 +0530

    added accounts page

--- flaskBlog/__init__.py
+++ flaskBlog/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+# ORM used to access the database.
 from flask_sqlalchemy import SQLAlchemy
+# used to encrpt the password of the user.
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -9,5 +12,7 @@ app.instance_path = 'flaskBlog/instance'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app) 
+login_manager.login_view = 'login'
 
 from flaskBlog import routes
--- flaskBlog/forms.py
+++ flaskBlog/forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField , BooleanField
+from flask_wtf.file import FileField , FileAllowed
+from wtforms import StringField, PasswordField, SubmitField , BooleanField, FileField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskBlog.models import User
+from flask_login import current_user
+
 
 class RegistationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(),Length(min=2,max=25)])
@@ -29,3 +32,25 @@ class LoginForm(FlaskForm):
     password = PasswordField('password', validators=[DataRequired()])
     remember = BooleanField('remember me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(),Length(min=2,max=25)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+    picture = FileField('Update Profile Picture' , validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+
+    # function to check username does not match with already existing data.
+    def validate_username(self,username):
+      if username.data != current_user.username:
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+          raise ValidationError("This username is already taken. please choose a different one")
+    
+    # function to check email does not match with already existing data.
+    def validate_email(self,email):
+      user = User.query.filter_by(email = email.data).first()
+
+      if email.data != current_user.email:
+        if user:
+          raise ValidationError("This email is already taken. please choose a different one")
